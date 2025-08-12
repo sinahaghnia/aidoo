a0c0ca9a30d0d4da317aea0a4284f60dc6b3604b
sina_haghnia <sinahaghnia1@gmil.com>
2025-08-12 15:53:35 +0330

    add advise and frequently question box

--- java script/index.js
+++ java script/index.js
@@ -1,4 +1,4 @@
-    //  add the element we need 
+    //  add the elements we need 
 
 const iconBar = document.querySelector('.navigation');
 const hiddenNav = document.querySelector('.hidden-navigation');
@@ -6,6 +6,18 @@ const backIcon = document.querySelector('.back-icon');
 const overLay = document.querySelector('.overlay');
 const login = document.querySelector('.log-in');
 const mainPage = document.querySelector('.main-page');
+
+const questionAndAnswer = document.querySelectorAll('.question-and-answer');
+
+const loginButton = document.querySelector('.login-button');
+
+const divNameInput = document.querySelector('.name-input');
+const divNumberInput = document.querySelector('.number-input');
+const nameInput = divNameInput.querySelector('input');
+const nameError = document.querySelector('.name-error');
+const numberInput = divNumberInput.querySelector('input');
+const numberError = document.querySelector('.number-error');
+
 
                             // when user click on navigation bar show slide bar 
 
@@ -20,9 +32,63 @@ backIcon.addEventListener('click' , (event)=>{
     overLay.classList.remove('show-overlay');
 })
 
-                        // when user click on login part af navigation go to the 'login.html'
+                            // make frequently question box functionality
 
-login.addEventListener('click' , ()=>{
-    window.location.href = 'login.html';
+questionAndAnswer.forEach( question =>{
+    const answer = question.querySelector('.answer')
+    const questionIcon = question.querySelector('.fa-angle-down')
+
+    question.addEventListener('click' , ()=>{
+
+        questionAndAnswer.forEach( q =>{
+        const otherQuestion = q.querySelector('.answer');
+
+        if( otherQuestion !== answer){
+            otherQuestion.classList.remove('show-answer');
+        };
+    })
+
+        answer.classList.toggle('show-answer');
+        questionIcon.classList.toggle('icon-rotation');
+    
+    })
 })
 
+                                        // when user click on login button go to login page
+
+loginButton.addEventListener('click' , ()=>{
+    window.location.href = 'login.html'
+})
+
+                                        // add functionality for advise part 
+
+
+nameInput.addEventListener('input' , ()=>{
+const value = nameInput.value;
+
+   if(value == ''){
+        nameInput.style.border = '2px solid #e7e4e4';
+        nameError.style.opacity = '0';
+
+    }else if(!isNaN(Number(value))){
+        nameInput.style.border = '1.5px solid red';
+        nameError.style.opacity = '1';
+    }else{
+        nameInput.style.border = '2px solid #0073ff'
+    }
+})
+
+numberInput.addEventListener('input' , ()=>{
+const value = numberInput.value;
+
+if(value === '' || value.startsWith('09') || value.startsWith('0') ){
+    numberInput.style.border = '2px solid #e7e4e4';
+    numberError.style.opacity = '0';
+
+}else{
+    numberInput.style.border = '2px solid red';
+    numberError.style.opacity = '1';
+}
+    
+
+})
